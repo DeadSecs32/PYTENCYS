958958bf9bf0a2f2a98509bdee2cc9b4b0bdc1dd
Gabriel Tomé Silveira <67930544+DeadSecs32@users.noreply.github.com>
2020-12-07 09:09:30 -0300

    Update main.py

--- PYTENCYS/main.py
+++ PYTENCYS/main.py
@@ -5,9 +5,11 @@ import relatorio as rl
 	Main.py: Será usado para chamar a matriz fornecida pelo usuário 
 	* biblioteca necessária: 
 		power_method: biblioteca principal
+		função: pm.metodo_das_potencias(prec = 1e-3) #prec: precisão do algoritimo 
 	*biblioteca opcional: 
 		relatorio: gera o relatório com os dados obtidos
 		na biblioteca power_method
+		função: rl.relat() #não precisa de argumentos. 
 '''
 
 pm.metodo_das_potencias(prec=1e-3)
